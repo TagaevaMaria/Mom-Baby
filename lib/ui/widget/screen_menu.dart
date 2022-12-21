392041a7a5d956cfa5a804219f0c3b0ccf1c049a
TagaevaMaria <vcebudet5.okyy@gmail.com>
2022-12-22 00:18:10 +0700

    рефакторинг кода на экране меню. Учусь переиспользовать созданный класс. Первые шаги к построению архитектуры.

--- lib/ui/widget/screen_menu.dart
+++ lib/ui/widget/screen_menu.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mom_and_baby/ui/navigation/main_navigation.dart';
 
 class ScreenMenu extends StatelessWidget {
   const ScreenMenu({Key? key}) : super(key: key);
@@ -7,39 +6,103 @@ class ScreenMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.transparent,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.cover, image: AssetImage('assets/images/fon.jpg')),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Padding(
-              padding: EdgeInsets.all(18.0),
-              child: RowOne(),
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.transparent,
+        body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    'assets/images/fon.jpg',
+                  )),
             ),
-            Padding(
-              padding: EdgeInsets.all(18.0),
-              child: Padding(
-                padding: EdgeInsets.all(18.0),
-                child: RowTwo(),
-              ),
-            ),
-            RowThree()
-          ],
-        ),
-      ),
-    );
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: MyRow(
+                    iconOne: Image.asset(
+                      'assets/images/doctor.png',
+                      height: 80,
+                      width: 80,
+                    ),
+                    iconTwo: Image.asset(
+                      'assets/images/development.png',
+                      height: 80,
+                      width: 80,
+                    ),
+                    iconThree: Image.asset('assets/images/height_weight.png',
+                        height: 80, width: 80),
+                    textOne: 'Запись к доктору',
+                    textTwo: 'Достижения',
+                    textThree: 'Рост и вес',
+                    oneFunction: () {},
+                    twoFunction: () {},
+                    threeFunction: () {},
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: MyRow(
+                    iconOne: Image.asset('assets/images/foto.png',
+                        height: 80, width: 80),
+                    textOne: 'Фото',
+                    iconTwo: Image.asset('assets/images/notes.png',
+                        height: 80, width: 80),
+                    textTwo: 'Заметки',
+                    iconThree: Image.asset('assets/images/allergy.png',
+                        height: 80, width: 80),
+                    textThree: 'Аллергия',
+                    oneFunction: () {},
+                    twoFunction: () {},
+                    threeFunction: () {},
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: MyRow(
+                    iconOne: Image.asset('assets/images/vaccinations.png',
+                        height: 80, width: 80),
+                    textOne: 'Прививки',
+                    iconTwo: Image.asset('assets/images/victories.png',
+                        height: 80, width: 80),
+                    textTwo: "Достижения",
+                    iconThree: Image.asset('assets/images/sleep.png',
+                        height: 80, width: 80),
+                    textThree: "Колыбельные",
+                    oneFunction: () {},
+                    twoFunction: () {},
+                    threeFunction: () {},
+                  ),
+                )
+              ],
+            )));
   }
 }
 
-/// первый ряд.
-class RowOne extends StatelessWidget {
-  const RowOne({
+class MyRow extends StatelessWidget {
+  final Image iconThree;
+  final String textOne;
+  final Image iconTwo;
+  final String textTwo;
+  final Image iconOne;
+  final String textThree;
+  final Function oneFunction;
+  final Function twoFunction;
+  final Function threeFunction;
+
+  const MyRow({
     Key? key,
+    required this.iconOne,
+    required this.textOne,
+    required this.iconTwo,
+    required this.textTwo,
+    required this.iconThree,
+    required this.textThree,
+    required this.oneFunction,
+    required this.twoFunction,
+    required this.threeFunction,
   }) : super(key: key);
 
   @override
@@ -52,24 +115,10 @@ class RowOne extends StatelessWidget {
           child: Column(
             children: [
               TextButton(
-                  onPressed: () => Navigator.of(context)
-                      .pushNamed(MainNavigatorNames.widgetDoctor),
-                  child: Image.asset('assets/images/doctor.png',
-                      height: 80, width: 80)),
-              const Text('Запись к врачу')
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextButton(
                 onPressed: () {},
-                child: Image.asset('assets/images/development.png',
-                    height: 80, width: 80),
+                child: iconOne,
               ),
-              const Text('Развитие')
+              Text(textOne)
             ],
           ),
         ),
@@ -77,40 +126,8 @@ class RowOne extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              TextButton(
-                onPressed: () {},
-                child: Image.asset('assets/images/height_weight.png',
-                    height: 80, width: 80),
-              ),
-              const Text('Рост, вес')
-            ],
-          ),
-        )
-      ],
-    );
-  }
-}
-
-/// второй ряд
-class RowTwo extends StatelessWidget {
-  const RowTwo({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextButton(
-                  onPressed: () {},
-                  child: Image.asset('assets/images/foto.png',
-                      height: 80, width: 80)),
-              const Text('Альбом')
+              TextButton(onPressed: () {}, child: iconTwo),
+              Text(textTwo)
             ],
           ),
         ),
@@ -118,78 +135,8 @@ class RowTwo extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              TextButton(
-                  onPressed: () {},
-                  child: Image.asset('assets/images/notes.png',
-                      height: 80, width: 80)),
-              const Text('Заметки')
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextButton(
-                onPressed: () {},
-                child: Image.asset('assets/images/allergy.png',
-                    height: 80, width: 80),
-              ),
-              const Text('Аллергия')
-            ],
-          ),
-        )
-      ],
-    );
-  }
-}
-
-/// третий ряд
-class RowThree extends StatelessWidget {
-  const RowThree({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextButton(
-                  onPressed: () {},
-                  child: Image.asset('assets/images/vaccinations.png',
-                      height: 80, width: 80)),
-              const Text('Прививки')
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextButton(
-                onPressed: () {},
-                child: Image.asset('assets/images/victories.png',
-                    height: 80, width: 80),
-              ),
-              const Text('Достижения')
-            ],
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextButton(
-                onPressed: () {},
-                child: Image.asset('assets/images/sleep.png',
-                    height: 80, width: 80),
-              ),
-              const Text('Колыбельные')
+              TextButton(onPressed: () {}, child: iconThree),
+              Text(textThree)
             ],
           ),
         )
